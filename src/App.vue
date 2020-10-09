44c8ec1c4cb91c18c5e4cf1e2160a7e735c20111
maxstain <firaschabchoub@hotmail.com>
2020-10-09 21:37:16 +0100

    feat: fixing the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <NavBar />
+  <LandingPage />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import LandingPage from "./components/LandingPage";
 
 export default {
   name: "App",
   components: {
     NavBar,
+    LandingPage,
   },
 };
 </script>
